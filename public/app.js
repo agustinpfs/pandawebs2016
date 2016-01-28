cb79eed8f7320c73c5fecde32a2b7ac37f4dadf2
agustinpfs <agustinpfs@gmail.com>
2016-01-28 13:28:51 -0300

    test transition

---
+++ public/app.js
@@ -0,0 +1,13 @@
+
+
+function moveServices() {
+$( ".servh2" ).toggleClass('test2, test')
+
+
+// $( ".body" ).addClass('ofly-h')
+// $( ".close" ).addClass('x')
+// $( ".Single-post" ).addClass('respo-post')
+
+}
+	
+$( ".servh2" ).on( "click", moveServices )
