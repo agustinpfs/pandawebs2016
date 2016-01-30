d81376c68b890cfc9cf6d98f02c3b767b993a149
agustinpfs <agustinpfs@gmail.com>
2016-01-30 11:08:37 -0300

    list divs & animation title

--- public/app.js
+++ public/app.js
@@ -6,6 +6,17 @@ $( ".technoh2" ).toggleClass('status1, status2')
 function moveServices() {
 $( ".servicesh2" ).toggleClass('status1--services, status2--services')
 }
+function moveContact() {
+$( ".contacth2" ).toggleClass('status1--contact, status2--contact')
+	setTimeout(function(){
+		$( ".Layout-h1" ).addClass('status2--logo')
+		$( ".contacth2" ).addClass('status3--contact')
+
+	}, 500);
+}
+
+
 	
 $( ".servicesh2" ).on( "click", moveServices )
 $( ".technoh2" ).on( "click", moveTechno )
+$( ".contacth2" ).on( "click", moveContact )
