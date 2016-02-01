c7fefdaa8b08f65cae795b4c6a226dbb479ce9a9
agustinpfs <agustinpfs@gmail.com>
2016-02-01 07:53:05 -0300

    add logo & animation form

--- public/app.js
+++ public/app.js
@@ -2,17 +2,24 @@
 
 function moveTechno() {
 $( ".technoh2" ).toggleClass('status1, status2')
+$( ".Layout-techno_content" ).fadeIn( 1000 )
 }
 function moveServices() {
 $( ".servicesh2" ).toggleClass('status1--services, status2--services')
+$( ".Layout-services_content" ).fadeIn( 1000 )
+
 }
 function moveContact() {
 $( ".contacth2" ).toggleClass('status1--contact, status2--contact')
 	setTimeout(function(){
 		$( ".Layout-h1" ).addClass('status2--logo')
 		$( ".contacth2" ).addClass('status3--contact')
+		setTimeout(function(){
+			$( ".Contact" ).slideDown( 500 )
+			$( ".icon-logo1" ).fadeIn( 1000 )
+		}, 1000)
 
-	}, 500);
+	}, 500)
 }
 
 
